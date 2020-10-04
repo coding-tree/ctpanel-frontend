28e1530838461013d2e1402792c7b19bc19effe7
Pawel Wojtkiewicz <pawel.wojtkiewicz66@gmail.com>
2020-10-04 21:17:40 +0200

    add amountOfPosts

--- src/api/getNearestMeetings.ts
+++ src/api/getNearestMeetings.ts
@@ -1,5 +1,7 @@
 export default async function getNearestMeetings<T>(): Promise<T> {
-    const response = await fetch(`https://api.ctpanel.pl/meetings/last?amount=3`, { credentials: 'include' });
+    let amountOfPosts = 3;
+
+    const response = await fetch(`https://api.ctpanel.pl/meetings/last?amount=${amountOfPosts}`, { credentials: 'include' });
     const body = await response.json();
     return body;
 };
